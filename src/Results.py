2350a085585dcf996b1fcc024cea6a69168c171a
mitchellhoesing <hoesingmitch02@gmail.com>
2024-06-16 02:33:26 -0400

    Almost done! huge commit

--- src/Results.py
+++ src/Results.py
@@ -1,24 +1,74 @@
+import numpy as np
+import json
+import re
 
 class Results:
-    def __init__(self, uid, year, title, response):
+    def __init__(self, uid, year, title, response, taxonomy, userBasePrompt, serializedResponse):
         self._uid = uid
         self._year = year
         self._title = title
         self._response = response
-        self._methodologyTaxonomy = ["brainstorming and focus groups", "interviews and questionnaires",
-                                     "conceptual modeling", "work diaries", "think-aloud sessions",
-                                     "shadowing and observation", "participant observation", "instrumenting systems",
-                                     "fly on the wall", "analysis of work databases", "analysis of tool use logs",
-                                     "documentation analysis", "static and dynamic analysis"]
-        self.classification = self._extractMethodologyClassification()
+        self._serializedResponse = serializedResponse
+        self._taxonomy = taxonomy
+        self.classifications = self._extractMethodologyClassification()
+        self.logprobs = self._extractTopLogProbs()
+        self._userBasePrompt = userBasePrompt
+
+    def __repr__(self):
+        result = f"\n{self._uid}, {self._year}, {self._title}, {self.classifications}\n"
+        result += str(self._response.choices[0].message.content) + "\n\n"
+        for logprob in self._response.choices[0].logprobs.content:
+            for top_logprob in logprob.top_logprobs:
+                result += f"Token: {str(top_logprob.token)} "
+                result += f"logprob: {str(top_logprob.logprob)} "
+                result += f"linear prob: {str(np.round(np.exp(top_logprob.logprob) * 100, 2))}\n"
+            result += "\n"
+        return result
 
     def appendToCSV(self, resultPath):
         with open(resultPath, 'a', encoding='utf-8-sig') as fd:
-            output = self._uid + "," + self._year + "," + self._title + "," + self.classification + "\n"
+            output = f"{self._uid},{self._year},{self._title},{self.classifications},{self.logprobs}," \
+                     f"\"{self._userBasePrompt}\"\n"
             fd.write(output)
 
+    def _extractTopLogProbs(self):
+        logprobs = ""
+        for logprob in self._response.choices[0].logprobs.content:
+            for top_logprob in logprob.top_logprobs:
+                logprobs += f"Token: {str(top_logprob.token)}, logprob: {str(top_logprob.logprob)}, "\
+                                f"linearprob: {str(np.round(np.exp(top_logprob.logprob) * 100, 2))} "
+
+        logprobs = self._replacePredictedNewlines(logprobs)
+        logprobs = self._replacePredictedQuotes(logprobs)
+        return f"\"{logprobs}\""
+
+    @staticmethod
+    def _replacePredictedNewlines(inputString):
+        outputString = inputString.replace('\n', '\\n')
+        return outputString
+
+    @staticmethod
+    def _replacePredictedQuotes(inputString):
+        outputString = inputString.replace('"', '')
+        return outputString
+
+    def appendToJSON(self, jsonPath):
+        with open(jsonPath, "w+", encoding='utf-8-sig') as json_file:
+            if not json_file:
+                jsonContents = json.load(json_file)
+            else:
+                jsonContents = []
+            jsonContents.append(self._serializedResponse)
+            json.dump(self._serializedResponse, json_file)
+
     def _extractMethodologyClassification(self):
-        self._response = self._response.lower()
-        for classification in self._methodologyTaxonomy:
-            if classification in self._response:
-                return classification
+        classifications = ""
+        self.responseText = self._response.choices[0].message.content
+        self.responseText = self.responseText.lower()
+        for classification in self._taxonomy:
+            if classification in self.responseText:
+                classifications += f"{classification},"
+        if not classifications: # if list is empty
+            return "\"DNE: " + self.responseText + "\""
+
+        return f"\"{classifications}\""
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import csv
 from openai import OpenAI
+from openai.types import CreateEmbeddingResponse
 import time
 import os
 from ResearchPaper import ResearchPaper
@@ -11,8 +12,8 @@ def main():
     rawData = r"data/rawData/chiData.csv"
     preprocessedData = r"data/preprocessedData/preprocessedChiData.csv"
     resultsPath = r"results/results.csv"
+    resultsJson = r"results/serializedResults.json"
 
-    # # Program Control TODO: Implement command line arguments.
     preprocess = False
     APICalls = True
 
@@ -32,20 +33,131 @@ def main():
         print('Skipping preprocessing...')
 
     if APICalls:
+        print("Making API calls...")
         client = OpenAI()
-        taxonomyPrompt = "Data methodology taxonomy: Brainstorming and focus groups, Interviews and questionnaires," \
-                         "Conceptual modeling, Work diaries, Think-aloud sessions, shadowing and observation, " \
-                         "Participant observation, Instrumenting systems, Fly on the wall, Analysis of work databases," \
-                         "Analysis of tool use logs, Documentation analysis, Static and dynamic analysis."
+        methodologyTaxonomy = "brainstorming and focus groups: In brainstorming, several people get together and " \
+                              "focus on a particular issue. The idea is to ensure that discussion is not limited to " \
+                              "“good” ideas or ideas that make immediate sense, but rather to uncover as many ideas " \
+                              "as possible. Focus Groups are similar to brainstorming. However, focus groups occur " \
+                              "when groups of people are brought together to focus on a particular issue (not just " \
+                              "generate ideas). They also involve moderators to focus the group discussion and make " \
+                              "sure that everyone has an opportunity to participate. " \
+                              "interviews: Interviews involve at least one researcher talking to at least one " \
+                              "respondent. Interviews can be conducted in two ways. In a structured interview, " \
+                              "a fixed list of carefully worded questions forms the basis of the interview. Usually, " \
+                              "the questions are asked exactly as written, and no deviations occur. The data from " \
+                              "structured interviews is usually analysed using statistical analyses. In a " \
+                              "semi-structured interview, the interview generally follows more of a conversational " \
+                              "flow. New questions may be devised as new information is learned." \
+                              "questionnaires: Questionnaires are sets of questions administered in a written or " \
+                              "digital format." \
+                              "conceptual modeling: During conceptual modeling, participants create a model of some " \
+                              "aspect of their work – the intent is to bring to light their mental models. In its " \
+                              "simplest form, participants draw a diagram of some aspect of their work. For instance, " \
+                              "software engineers may be asked to draw a data flow diagram, a control flow diagram or " \
+                              "a package diagram showing the important architectural clusters of their system. As an " \
+                              "orthogonal usage, software engineers may be asked to draw a physical map of their " \
+                              "environment, pointing out who they talk to and how often." \
+                              "work diaries: Work diaries require respondents to record various events that occur " \
+                              "during the day. It may involve filling out a form at the end of the day, " \
+                              "recording specific activities as they occur, or noting whatever the current task is at " \
+                              "a pre-selected time. These diaries may be kept on paper or in a computer." \
+                              "think-aloud sessions: In think-aloud protocol analysis, researchers ask participants " \
+                              "to think out loud while performing a task. The task can occur naturally at work or be " \
+                              "predetermined by the researcher." \
+                              "shadowing: In shadowing, the experimenter follows the participant around and records " \
+                              "their activities. Shadowing can occur for an unlimited time period, as long as there " \
+                              "is a willing participant." \
+                              "participant-observer: Usually done as part of an ethnography, in the " \
+                              "participant-observer technique, the researcher essentially becomes part of the team " \
+                              "and participates in key activities." \
+                              "instrumenting systems: This technique requires “instrumentation” to be built into the " \
+                              "software tools used by the software engineer. This instrumentation is used to record " \
+                              "information automatically about the usage of the tools. Instrumentation can be used to " \
+                              "monitor how frequently a tool or feature is used, patterns of access to files and " \
+                              "directories, and even the timing underlying different activities. This technique is " \
+                              "also called system monitoring. In some cases, instrumentation merely records the " \
+                              "commands issued by users. More advanced forms of instrumentation record both the input " \
+                              "and output in great detail so that the researcher can effectively play back the " \
+                              "session." \
+                              "fly on the wall: “Fly on the Wall” is a hybrid technique. It allows the researcher to " \
+                              "be an observer of an activity without being present. Participants are asked to video " \
+                              "or audiotape themselves when they are engaged in some predefined activity." \
+                              "document corpus analysis: This is the analysis of data corpora derived from textual " \
+                              "data such as electronic databases of performed work, tool logs, commit histories, " \
+                              "code, repositories, documentation, and website scraping." \
+                              "social media analysis: This is the analysis of data deriving from social media " \
+                              "websites. This could be from scraping social media websites or using their APIs." \
+                              "image, audio, and video library analysis: This is the analysis of a collection of " \
+                              "image, audio, and/or video files not created by the researchers in the research " \
+                              "paper." \
+                              "lab experiment: These are conducted under controlled conditions, in which the " \
+                              "researcher deliberately changes something (independent variable) to see the effect of " \
+                              "this on something else (dependent variable)" \
+                              "no experiment: These are research papers that did not perform a experiment." \
+            # Added: social media analysis, website analysis, online surveys and polls for amazon mechanical turk/crowd workers.
+        # Split: interviews and questionnaires
+        # "work database analysis, "
+        # "tool use log analysis," \ Combined work database analysis, tool use log analysis and documentation analysis.
+        # "static and dynamic analysis" Not going to see in CHI.
 
+        # sourceTaxonomy = "mouse and keyboard interaction data, " \
+        #                  "touch interaction data, " \
+        #                  "voice interaction data, " \
+        #                  "gaze tracking data, " \
+        #                  "gesture data, " \
+        #                  "other sensor data, " \
+        #                  "musical data, " \
+        #                  "video data, " \
+        #                  "video games data, " \
+        #                  "augmented or virtual reality data, " \
+        #                  "facebook social media data, " \
+        #                  "twitter social media data, " \
+        #                  "youtube social media data, " \
+        #                  "reddit social media data, " \
+        #                  "other social media data, " \
+        #                  "web scraped data, " \
+        #                  "internet-of-things data, " \
+        #                  "design artifact data, " \
+        #                  "revision history data, " \
+        #                  "physiological data, " \
+        #                  "emotional response data, " \
+        #                  "ethnographic data, " \
+        #                  "system log data, " \
+        #                  "team chat log data, " \
+        #                  "not clear"
+        taxonomy = ['brainstorming and focus groups', 'interviews', 'questionnaires', 'conceptual modeling',
+                    'word diaries', 'think-aloud sessions', 'shadowing', 'participant-observer', 'instrumenting systems'
+                    , 'fly on the wall', 'document corpus analysis', 'social media analysis',
+                    'image audio and video library analysis', 'lab experiment', 'no experiment']
+        taxonomy = list(map(str.strip, taxonomy))
+
+        taxonomyPrompt = f"Data gathering methodology taxonomy and taxa definitions: {methodologyTaxonomy}"
+        userBasePrompt = "What are the data gathering methodologies of this research paper according to our taxonomy? " \
+                         "Answer in the format of the following " \
+                         "sentence. The set of this research paper's data gathering " \
+                         "methodologies contains [MASK]."
+        # Classify the data gathering method(s) into any applicable classes in the data methodology gathering taxonomy.
+
+        # "You are to perform a single or multiclass classification of the provided research " \
+        # "paper's data gathering methodology.
+
+        # filling in the [MASK] token with a set of any length containing methodologies that match best.
         with open(preprocessedData, newline='', encoding="utf-8-sig") as rawText:
             paperReader = csv.reader(rawText)
             next(paperReader)
             for row in paperReader:
+                # if paperReader.line_num != 6455 + 1: # Dodge starcraft paper
+
+                # if paperReader.line_num != 6250 + 1: # binns paper. group truth: interviews, lab-based studies, online experiments, and think-aloud sessions.
+
+                # if paperReader.line_num != 882 + 1:
+
                 uid = row[0]
                 year = row[1]
                 title = row[2]
                 text = row[3]
+
                 '''
                 Roles: 
                 System: The system message helps set the behavior of the assistant. For example, you can modify the personality
@@ -63,22 +175,25 @@ def main():
                   model considers the results of the tokens with top_p probability mass. So 0.1
                   means only the tokens comprising the top 10% probability mass are considered.
                 '''
+
+                userFinalPrompt = f"Your task is to classify the data gathering methodology of the following research " \
+                                  f"paper into our provided data gathering " \
+                                  f"methodology taxonomy. Classification: Data gathering methodology - " \
+                                  f"Research paper:\n{text}\n{taxonomyPrompt}\n{userBasePrompt} "
+
                 response = client.chat.completions.create(
                     model="gpt-3.5-turbo-0125",
                     messages=[
-                        # {"role": "system", "content": "You are to classify the provided research paper's data source "
-                        #                               "and data gathering methodology into the provided taxonomy. "
-                        #                               "Respond with only your classifications.\n" + taxonomyPrompt +
-                        #                               "Research paper:\n" + text},
-                        {"role": "user", "content": "Research paper:\n" + text + "\n" + taxonomyPrompt +
-                                                    "\nClassification: Data source and data gathering methodology - "
-                                                    "You are to classify the provided research paper's data source and "
-                                                    "data gathering methodology into one category in the provided "
-                                                    "taxonomy, in the format of the following sentence, filling in the "
-                                                    "[MASK] tokens."
-                                                    "This research paper utilizes [MASK] as a data source and [MASK] "
-                                                    "is the data gathering methodology."}
-                         #} Respond with only your classification.
+                        {"role": "system", "content": "You are a professional CHI research scientist."},
+                        {"role": "user", "content": userFinalPrompt}
+                        # If the data source is not clear then "
+                        #                                      "answer "
+                        # \"The data source is not listed in the above data source "
+                        #  "taxonomy.\"
+                        # "utilizes [MASK] as a data source and [MASK] is the data "
+                        # "gathering methodology."}
+                        # The data source is not listed in the above data source taxonomy.
+                        # } Respond with only your classification.
 
                     ],
                     # Temperature = 0 to favor highest probability token in generation
@@ -90,15 +205,12 @@ def main():
                     logprobs=True,
                     top_logprobs=5
                 )
-                classification = response.choices[0].message.content
-                results = Results(uid, year, title, classification)
+                serializedResponse = response.model_dump_json()
+                results = Results(uid, year, title, response, taxonomy, userBasePrompt, serializedResponse)
                 results.appendToCSV(resultsPath)
+                results.appendToJSON(resultsJson)
 
-
-                print(f"{uid}, {year}, {title}, {classification}")
-                print(response.choices[0].logprobs.content)
-                print(response)
-                break
+                # print(results)
 
     else:
         print("Skipping API Calls")
